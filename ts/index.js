ff1f65075e0b139f48a92009bddafa2afcca022f
mallasorte12 <mallasorte12@gmail.com>
2022-04-13 13:49:38 +0200

    calculator-done

--- ts/index.js
+++ ts/index.js
@@ -1,1 +1,43 @@
-console.log("typing in typescript hahah");
+"use strict";
+exports.__esModule = true;
+var readline_sync_1 = require("readline-sync");
+function main() {
+    var run = true;
+    var runStr;
+    while (run) {
+        var firstStr = (0, readline_sync_1.question)("Enter the first number:\n");
+        var operator = (0, readline_sync_1.question)("Enter the operator:\n");
+        var secondStr = (0, readline_sync_1.question)("Enter the second number:\n");
+        console.log(firstStr, operator, secondStr);
+        console.log(calculate(firstStr, operator, secondStr));
+        runStr = (0, readline_sync_1.question)("Say \"Y\" if you wish to continue.\n");
+        run = decide(runStr);
+    }
+}
+function isNumber(num) {
+    return typeof parseInt(num) === "number";
+}
+function calculate(num1, operator, num2) {
+    var operators = ["+", "-", "*", "/"];
+    console.log("Index of operator: " + operators.indexOf(operator));
+    if (isNumber(num1) && isNumber(num2) && (operators.indexOf(operator) + 1)) {
+        var first = parseInt(num1);
+        var second = parseInt(num2);
+        console.log(first, second);
+        switch (operator) {
+            case "+":
+                return num1 + " + " + num2 + " = " + (first + second);
+            case "-":
+                return num1 + " - " + num2 + " = " + (first - second);
+            case "*":
+                return num1 + " * " + num2 + " = " + (first * second);
+            case "/":
+                return num1 + " / " + num2 + " = " + (first / second);
+        }
+    }
+    return "The input is not valid. Try once again.";
+}
+function decide(option) {
+    return option === "Y";
+}
+main();
--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,59 @@
-console.log("typing in typescript hahah");
+import { question } from 'readline-sync';
+
+function main(): void
+{
+    let run: boolean = true;
+    let runStr: string;
+    while (run){
+        const firstStr: string = question("Enter the first number:\n");
+        const operator: string = question("Enter the operator:\n");
+        const secondStr: string = question("Enter the second number:\n");
+
+        console.log(firstStr, operator, secondStr);
+        console.log(calculate(firstStr, operator, secondStr));
+
+        runStr = question("Say \"Y\" if you wish to continue.\n");
+        run = decide(runStr);
+    }
+
+}
+
+function isNumber(num: string): boolean
+{
+    return typeof parseInt(num) === "number";
+}
+
+function calculate(num1: string, operator: string, num2: string): string
+{
+    const operators: Array<string> = ["+", "-", "*", "/"];
+
+
+    if (isNumber(num1) && isNumber(num2) && (operators.indexOf(operator) + 1)){
+
+        const first: number = parseInt(num1);
+        const second: number = parseInt(num2);
+
+        switch (operator){
+            case "+":
+                return num1 + " + " + num2 + " = " + (first + second);
+
+            case "-":
+                return num1 + " - " + num2 + " = " + (first - second);
+
+            case "*":
+                return num1 + " * " + num2 + " = " + (first * second);
+
+            case "/":
+                return num1 + " / " + num2 + " = " + (first / second);
+        }
+    }
+    return "The input is not valid. Try once again.";
+
+}
+
+function decide(option: string): boolean
+{
+    return option === "Y";
+}
+
+main();
